94c48efb0698b69e43cc27aea2e68162e98aec70
Noé <noe_juarez@epam.com>
2022-05-13 12:37:45 -0500

    [Feat] Queue Class

---
+++ classes/queue.js
@@ -0,0 +1,22 @@
+
+class Queue {
+    #queue = []
+
+    constructor() {}
+
+    get queue()     { return this.#queue; }
+
+    offer(item)      { this.#queue.push(item); }
+
+    poll()           { return this.#queue.shift(); }
+
+    peek()          { return this.#queue[0] }
+
+    length()        { return this.#queue.length; }
+
+    isEmpty()       { return !this.#queue.length; }
+
+    search(item)    { return this.#queue.indexOf(item); }
+}
+
+module.exports = { Queue }
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const { Stack } = require("./classes/stack");
+const { Queue } = require("./classes/queue");
 
 let newStack = new Stack();
+let newQueue = new Queue();
